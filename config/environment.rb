f7571b8f74344fb1541134499e35e982f189decf
coding4medicine <manoj.samanta@gmail.com>
2016-08-13 16:20:44 +0000

    devise+email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+       :user_name => ENV['MAIL_EMAIL'],
+       :password => ENV['MAIL_PWD'],
+       :domain => 'gmail.com',
+       :address => 'smtp.gmail.com',
+       :port => 587,
+       :authentication => :plain,
+       :enable_starttls_auto => true
+}
+
