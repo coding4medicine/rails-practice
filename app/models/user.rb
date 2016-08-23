581c469bc3c17169acd40db7076bc360d3331391
coding4medicine <manoj.samanta@gmail.com>
2016-08-23 20:52:18 +0000

    ID to id

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
     has_many :sshkeys
     has_many :cards
 
-    has_many :books, through => :book_purchases
+    has_many :books, through: :book_purchases
     has_many :book_purchases
 
-    has_many :plans, through => :plan_purchases
+    has_many :plans, through: :plan_purchases
     has_many :plan_purchases
 end
