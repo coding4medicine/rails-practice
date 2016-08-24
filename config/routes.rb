68c0c85097016f2d4559e7fc6e7e944ff04c032a
coding4medicine <manoj.samanta@gmail.com>
2016-08-24 02:14:00 +0000

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  resources :book_purchases
-  resources :cards
-  resources :plan_purchases
-  resources :stripe_events
-  resources :referrals
-  resources :plans
-  resources :books
+
   resources :sshkeys
-  devise_for :admins
+
+  scope module: 'admin' do
+     resources :plans, :books, :cards, :referrals, :keys
+  end
+
   get 'pages/page0'
-
   get 'pages/page1'
-
   get 'pages/page2'
+  get 'pages/book'
 
+  root :to => 'pages#index'
+
+  devise_for :admins
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
